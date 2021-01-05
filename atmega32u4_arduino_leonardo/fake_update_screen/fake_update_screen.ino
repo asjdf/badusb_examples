d07dcac3db1b8b941735a512f38fe6ad01d4be60
杨成锴 <243852814@qq.com>
2021-01-06 00:17:51 +0800

    Update fake_update_screen.ino

--- atmega32u4_arduino_leonardo/fake_update_screen/fake_update_screen.ino
+++ atmega32u4_arduino_leonardo/fake_update_screen/fake_update_screen.ino
@@ -2,15 +2,16 @@
 #define kbd_es_es
 
 void setup() {
-  Keyboard.begin();
+    Keyboard.begin();
 }
 
 void loop() {
-  delay(5000);
-  CommandAtRunBarMSWIN("iexplore -k http://fakeupdate.net/win10u/index.html");
-  delay(5000);
-  Keyboard.press(KEY_F11);
-  delay(100);
-  Keyboard.releaseAll();
-  while(1);
+    delay(5000);
+    CommandAtRunBarMSWIN("iexplore -k https://fakeupdate.net/win10ue/");
+    delay(5000);
+    Keyboard.press(KEY_F11);
+    delay(100);
+    Keyboard.releaseAll();
+    while (1)
+        ;
 }
